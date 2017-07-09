879d70075fcb5318e246df044d476a78bae74a07
ashui <ashui@ashuihouse.cn>
2017-07-09 20:45:34 +0800

    practice 2 and 3 with 1

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,5 +1,63 @@
 'use strict';
 
-function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+function countSameElements(collection) 
+{
+	var result=[{}];
+	var n=0;
+	var count=1;
+	var reg=new Boolean();
+	for(var i=0;i<collection.length;i++)
+	{
+		if(collection[i].length!=1)
+		{
+			for(var j=0;j<result.length;j++)
+			{
+				if(collection[i][0]==result[j].name)
+				{
+					result[j].summary+=parseInt(collection[i].match(/\d+/g)[0]);
+					reg=false;
+				}
+
+			}
+			if(reg)
+			{
+				result[n]=new Object();
+				result[n].name=collection[i][0];
+				result[n].summary=parseInt(collection[i].match(/\d+/g)[0]);
+				count=1;
+				n++;
+			}
+			reg=true;
+		}
+		else
+		{	
+			for(var j=0;j<result.length;j++)
+			{
+				if(collection[i]==result[j].name)
+				{			
+					result[j].summary++;
+					reg=false;
+				}
+			}
+			if(reg)
+			{
+				if(collection[i]==collection[i+1])
+				{			
+					count++;
+				}
+				else
+				{
+					result[n]=new Object();
+					result[n].name=collection[i];
+					result[n].summary=count;
+					count=1;
+					n++;
+				}
+			}
+			reg=true;
+		}
+
+	}
+	return result;
 }
+
