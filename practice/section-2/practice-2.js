3090a5b62636cef4911533d55deb3b35f8e679aa
ashui <ashui@ashuihouse.cn>
2017-07-09 18:28:17 +0800

    practice2 and 2 with1

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,29 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+	var result=new Array();
+	var n=0;
+	var count=1;
+	var reg=new Array();
+	var key=new String();
+	for(var i=0;i<collection.length;i++){
+		if(collection[i].length!=1){
+			reg=collection[i].split('');
+			count+=parseInt(reg[2])-1;
+			key=reg[0];
+		}
+		if(collection[i]==collection[i+1]){			
+			count++;
+			key=collection[i];
+		}
+		else{
+			result[n]=new Object();
+			result[n].key=key;
+			result[n].count=count;
+			count=1;
+			n++;
+		}
+	}
+	return result;
 }
+
