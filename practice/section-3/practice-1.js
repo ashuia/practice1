a84aca739768aa049e8afb1a5bdf7bcf5618ca66
ashui <ashui@ashuihouse.cn>
2017-07-09 21:31:04 +0800

    practice 3 and 1 with 1

--- practice/section-3/practice-1.js
+++ practice/section-3/practice-1.js
@@ -1,5 +1,16 @@
 'use strict';
 
-function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+function createUpdatedCollection(collectionA, objectB) 
+{
+ 	for(var i=0;i<collectionA.length;i++)
+ 	{
+ 		for(var j=0;j<objectB.value.length;j++)
+ 		{
+ 			if(collectionA[i].key==objectB.value[j])
+ 			{
+ 				collectionA[i].count-=1;
+ 			}
+ 		}
+ 	}
+ 	return collectionA;
 }
